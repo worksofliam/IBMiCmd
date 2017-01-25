a8bac4c75d04b7eba22cba5f635c59a87c149922
Liam Allan <mrliamallan@live.co.uk>
2017-01-25 21:34:31 +0000

    Support for SQL programs

--- ErrorHandle.cs
+++ ErrorHandle.cs
@@ -46,17 +46,53 @@ namespace IBMiCmd
         public static void wrkErrors()
         {
             _Errors = new List<lineError>();
+
             string err;
             int sev;
+            int linenum, sqldiff;
+
+            List<expRange> exps = new List<expRange>();
+            string[] pieces;
+            string curtype;
 
             foreach (string line in _Lines)
             {
                 if (line == null) continue;
                 err = line.PadRight(150);
-                if (err.Substring(0, 10).TrimEnd() == "ERROR")
+                curtype = err.Substring(0, 10).TrimEnd();
+                switch(curtype)
                 {
-                    sev = int.Parse(err.Substring(58, 2));
-                    _Errors.Add(new lineError(sev, err.Substring(37, 6), err.Substring(65)));
+                    case "EXPANSION":
+                        pieces = err.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        MessageBox.Show(pieces[6] + " - " + pieces[7]);
+                        exps.Add(new expRange(int.Parse(pieces[6]), int.Parse(pieces[7])));
+
+                        break;
+                    case "ERROR":
+                        sev = int.Parse(err.Substring(58, 2));
+                        linenum = int.Parse(err.Substring(37, 6));
+                        sqldiff = 0;
+
+                        if (sev >= 10)
+                        {
+                            MessageBox.Show("exps: " + exps.Count);
+                            foreach (expRange range in exps)
+                            {
+                                if (range.inRange(linenum))
+                                {
+                                    sqldiff += range.getVal();
+                                }
+                            }
+                        }
+
+                        if (sqldiff > 0)
+                        {
+                            MessageBox.Show(linenum + " - " + sqldiff);
+                            linenum -= sqldiff;
+                        }
+
+                        _Errors.Add(new lineError(sev, linenum.ToString(), err.Substring(65)));
+                        break;
                 }
             }
         }
@@ -64,6 +100,29 @@ namespace IBMiCmd
         public static lineError[] getErrors()
         {
             return _Errors.ToArray();
+        }
+    }
+
+    class expRange
+    {
+        public int _low;
+        public int _high;
+
+        public expRange(int low, int high)
+        {
+            _low = low;
+            _high = high;
+            MessageBox.Show(getVal().ToString());
+        }
+
+        public bool inRange(int num)
+        {
+            return (num >= _high);
+        }
+
+        public int getVal()
+        {
+            return (_high - _low) + 1;
         }
     }
 }
