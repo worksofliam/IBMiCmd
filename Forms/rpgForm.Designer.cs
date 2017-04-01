c56f17ceea8a7a3e0269c4c77d09c0e986b62d91
Liam Allan <mrliamallan@live.co.uk>
2017-04-01 21:58:09 +0100

    rpg conversion now supporting more types

--- Forms/rpgForm.Designer.cs
+++ Forms/rpgForm.Designer.cs
@@ -42,7 +42,7 @@
             this.textBox1.Location = new System.Drawing.Point(12, 31);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(443, 26);
+            this.textBox1.Size = new System.Drawing.Size(726, 26);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
@@ -50,7 +50,7 @@
             this.textBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(12, 82);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(443, 26);
+            this.textBox2.Size = new System.Drawing.Size(726, 26);
             this.textBox2.TabIndex = 1;
             // 
             // label1
@@ -73,27 +73,32 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(348, 135);
+            this.button1.Location = new System.Drawing.Point(631, 134);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Accept Changes";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button2.Location = new System.Drawing.Point(12, 135);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Decline Changes";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // rpgForm
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 169);
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(753, 169);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
--- Forms/rpgForm.cs
+++ Forms/rpgForm.cs
@@ -25,95 +25,141 @@ namespace IBMiCmd.Forms
             input = ' ' + input.PadRight(80);
             char[] chars = input.ToCharArray();
 
-            string name = "";
+            string name = input.Substring(7, 16).Trim();
             string len = "";
             string type = "";
             string decimals = "";
-            string keywords = "";
+            string keywords = input.Substring(44).Trim();
             string output = "";
+            string field = "";
 
-            if (Char.ToUpper(chars[6]) == 'D')
+            switch(Char.ToUpper(chars[6]))
             {
-                name = input.Substring(7, 16).Trim();
-                len = input.Substring(33, 7).Trim();
-                type = input.Substring(40, 1);
-                decimals = input.Substring(41, 3).Trim();
-                keywords = input.Substring(44).Trim();
+                case 'D':
+                    len = input.Substring(33, 7).Trim();
+                    type = input.Substring(40, 1).Trim();
+                    decimals = input.Substring(41, 3).Trim();
+                    field = input.Substring(24, 2).Trim().ToUpper();
 
-                switch (type.ToUpper())
-                {
-                    case "A":
-                        if (keywords.ToUpper().Contains("VARYING"))
-                        {
-                            type = "Varchar";
-                        }
-                        else
-                        {
-                            type = "Char";
-                        }
-                        type += "(" + len + ")";
-                        break;
-                    case "B":
-                        type = "Bindec" + "(" + len + ")";
-                        break;
-                    case "C":
-                        type = "Ucs2" + "(" + len + ")";
-                        break;
-                    case "D":
-                        type = "Date";
-                        break;
-                    case "F":
-                        type = "Float" + "(" + len + ")";
-                        break;
-                    case "G":
-                        if (keywords.ToUpper().Contains("VARYING"))
-                        {
-                            type = "Graph";
-                        }
-                        else
-                        {
-                            type = "Vargraph";
-                        }
-                        type += "(" + len + ")";
-                        break;
-                    case "I":
-                        type = "Int" + "(" + len + ")";
-                        break;
-                    case "N":
-                        type = "Ind";
-                        break;
-                    case "P":
-                        type = "Packed" + "(" + len + ":" + decimals + ")";
-                        break;
-                    case "S":
-                        type = "Zoned" + "(" + len + ":" + decimals + ")";
-                        break;
-                    case "T":
-                        type = "Time";
-                        break;
-                    case "U":
-                        type = "Uns" + "(" + len + ")";
-                        break;
-                    case "Z":
-                        type = "Timestamp";
-                        break;
-                    case "*":
-                        type = "Pointer";
-                        break;
+                    switch (type.ToUpper())
+                    {
+                        case "A":
+                            if (keywords.ToUpper().Contains("VARYING"))
+                            {
+                                type = "Varchar";
+                            }
+                            else
+                            {
+                                type = "Char";
+                            }
+                            type += "(" + len + ")";
+                            break;
+                        case "B":
+                            type = "Bindec" + "(" + len + ")";
+                            break;
+                        case "C":
+                            type = "Ucs2" + "(" + len + ")";
+                            break;
+                        case "D":
+                            type = "Date";
+                            break;
+                        case "F":
+                            type = "Float" + "(" + len + ")";
+                            break;
+                        case "G":
+                            if (keywords.ToUpper().Contains("VARYING"))
+                            {
+                                type = "Graph";
+                            }
+                            else
+                            {
+                                type = "Vargraph";
+                            }
+                            type += "(" + len + ")";
+                            break;
+                        case "I":
+                            type = "Int" + "(" + len + ")";
+                            break;
+                        case "N":
+                            type = "Ind";
+                            break;
+                        case "P":
+                            type = "Packed" + "(" + len + ":" + decimals + ")";
+                            break;
+                        case "S":
+                            type = "Zoned" + "(" + len + ":" + decimals + ")";
+                            break;
+                        case "T":
+                            type = "Time";
+                            break;
+                        case "U":
+                            type = "Uns" + "(" + len + ")";
+                            break;
+                        case "Z":
+                            type = "Timestamp";
+                            break;
+                        case "*":
+                            type = "Pointer";
+                            break;
+                        case "":
+                            if (len != "")
+                            {
+                                if (decimals == "")
+                                {
+                                    if (keywords.ToUpper().Contains("VARYING"))
+                                    {
+                                        type = "Varchar";
+                                    }
+                                    else
+                                    {
+                                        type = "Char";
+                                    }
+                                    type += "(" + len + ")";
+                                }
+                                else
+                                {
+                                    type = "Zoned" + "(" + len + ":" + decimals + ")";
+                                }
+                            }
+                            break;
+                    }
 
-                    default:
-                        MessageBox.Show("A data-type is required to convert.", "Conversion error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return "";
-                }
+                    switch (field)
+                    {
+                        case "S":
+                            output = "Dcl-S " + name + " " + type + " " + keywords + ';';
+                            break;
+                        case "DS":
+                        case "PR":
+                        case "PI":
+                            if (name == "") name = "*N";
+                            output = "Dcl-" + field + " " + name + " " + type + " " + keywords + ";";
+                            break;
+                        case "":
+                            if (name == "") name = "*N";
+                            output = "  Dcl-Parm " + name + " " + type + " " + keywords + ';';
+                            break;
+                    }
+                    break;
 
-                output = "Dcl-S " + name + " " + type + " " + keywords + ';';
+                case 'P':
+                    switch(Char.ToUpper(chars[24]))
+                    {
+                        case 'B':
+                            output = "Dcl-Proc " + name + " " + keywords + ";";
+                            break;
+                        case 'E':
+                            output = "End-Proc;";
+                            break;
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Specification not supported by converter.", "Conversion error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
             }
-            else
-            {
-                MessageBox.Show("Definition specification is required to convert.", "Conversion error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
 
-            return output;
+            return "".PadLeft(8) + output;
         }
 
         private void rpgForm_Load(object sender, EventArgs e)
@@ -141,12 +187,30 @@ namespace IBMiCmd.Forms
             int lineLength = (int)Win32.SendMessage(curScintilla, SciMsg.SCI_LINELENGTH, line, 0);
             StringBuilder sb = new StringBuilder(lineLength);
             Win32.SendMessage(curScintilla, SciMsg.SCI_GETLINE, line, sb);
+
+            line = (int)Win32.SendMessage(curScintilla, SciMsg.SCI_POSITIONFROMLINE, line, 0);
+            lineLength--;
+            Win32.SendMessage(curScintilla, SciMsg.SCI_SETSELECTION, line, line+lineLength);
+
             return sb.ToString().Substring(0, lineLength);
         }
 
-        private static void setLine(int line, int value)
+        private static void setLine(string value)
         {
+            //Hopefully is still selected?
+            IntPtr curScintilla = PluginBase.GetCurrentScintilla();
+            Win32.SendMessage(curScintilla, SciMsg.SCI_REPLACESEL, 0, value);
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setLine(textBox2.Text);
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
