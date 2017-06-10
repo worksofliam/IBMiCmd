9e73c7990287a1186e7551b1355f528c36cdffe1
Mathias Langlet <mlanglet@kth.se>
2017-06-10 03:14:44 +0200

    initial commit on CL prompt client side

---
+++ LanguageTools/CLCommandPrompter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using IBMiCmd.IBMiTools;
+using NppPluginNET;
+
+namespace IBMiCmd.LanguageTools
+{
+    class CLCommandPrompter
+    {
+        /// <summary>
+        /// Uses information about position of the cursor and cached CL commands
+        /// Provides a window where the user can enter parameters for a CL command
+        /// </summary>
+        internal static void ProvideCommandHelp()
+        {
+            IBMiUtilities.DebugLog($"ProvideCommandHelp");
+            IntPtr curScintilla = PluginBase.GetCurrentScintilla();
+
+            int curLine = (int)Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETCURRENTLINE, 0, 0);
+            int lineLength = (int)Win32.SendMessage(curScintilla, SciMsg.SCI_LINELENGTH, curLine, 0);
+
+            if (lineLength <= 0) return;
+
+            StringBuilder sb = new StringBuilder(lineLength);
+            Win32.SendMessage(curScintilla, SciMsg.SCI_GETLINE, curLine, sb);
+
+            int cursorPosition = (int)Win32.SendMessage(curScintilla, SciMsg.SCI_GETCURRENTPOS, 0, 0);
+            int lineOffsetPosition = (int)Win32.SendMessage(curScintilla, SciMsg.SCI_POSITIONFROMLINE, curLine, 0);
+            int linePosition = cursorPosition - lineOffsetPosition;
+            if (linePosition == 0) return;
+
+            StringBuilder fileExtension = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETEXTPART, Win32.MAX_PATH, fileExtension);
+
+            string lookupString = CLParser.GetCommandAtColumn(sb.ToString(), linePosition);
+            //switch (fileExtension.ToString())
+            //{
+            //    case "rpgle":
+            //    case "sqlrpgle":
+            //        lookupString = RPGParser.GetVariableAtColumn(sb.ToString(), linePosition, out MatchType typeOfMatch);
+            //        break;
+            //    case "clp":
+            //    case "clle":
+            //        lookupString = CLParser.GetCommandAtColumn(sb.ToString(), linePosition);
+            //        break;
+            //    default:
+            //        // TODO: ?
+            //        break;
+            //}
+
+
+            IBMiUtilities.DebugLog($"ProvideCommandHelp - {lookupString.ToUpper()}");
+            if (lookupString == "") return;
+
+            GetCommandHelp(lookupString.ToUpper());
+            IBMiUtilities.DebugLog($"ProvideCommandHelp End");
+        }
+
+        private static void GetCommandHelp(string c)
+        {
+            IBMiUtilities.DebugLog($"GetCommandHelp");
+            bool cacheHit = false;
+            CLCommand command = new CLCommand();
+            foreach (CLCommand cmd in CLParser.CLCommands)
+            {
+                if (cmd.Equals(c))
+                {
+                    cacheHit = true;
+                    command = cmd;
+                    break;
+                }
+            }
+
+            if (cacheHit)
+            {
+                IBMiUtilities.DebugLog($"Cache hit for {c}");
+                //DisplayCommandHelp(command);
+            }
+            else
+            {
+                IBMiUtilities.DebugLog($"Cache miss for {c}");
+                //DisplayLoadingScreen();
+                FetchCommandDefinition(c);
+                //DisplayCommandHelp(CLParser.CLCommands[CLParser.CLCommands.Count - 1]);
+            }
+            IBMiUtilities.DebugLog($"GetCommandHelp End");
+        }
+
+        private static void DisplayLoadingScreen()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayCommandHelp(CLCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FetchCommandDefinition(string command)
+        {
+            Thread thread = new Thread((ThreadStart)delegate
+            {
+                IBMiUtilities.DebugLog($"FetchCommandDefinition");
+                // Receive command description via IICRTVCMD
+                IBMi.RunCommands(IBMiCommandRender.RenderCommandDescriptionCollection(command));
+
+                try
+                {
+                    CLParser.LoadCMD(command);
+                }
+                catch (Exception e)
+                {
+                    IBMiUtilities.Log(e.ToString()); // TODO: Show error?
+                }
+                IBMiUtilities.DebugLog($"FetchCommandDefinition End");
+            });
+            thread.Start();
+        }
+    }
+}
---
+++ LanguageTools/CLParser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using IBMiCmd.IBMiTools;
+
+namespace IBMiCmd.LanguageTools
+{
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct CLCommand
+    {
+        public string command;
+
+        public CLCommand(string name)
+        {
+            command = name;
+        }
+
+        public bool Equals(String c)
+        {
+            return this.command == c;
+        }
+    }
+
+    public class CLParser
+    {
+        // In memory cache of cl commands
+        public static List<CLCommand> CLCommands { get; set; }
+
+        public static string GetCommandAtColumn(string curLine, int curPos)
+        {
+            if (curPos > curLine.Length) return "";
+            StringBuilder sb = new StringBuilder();
+
+            // Scan Backwards
+            for (int i = curPos; i >= 0; i--)
+            {
+                if (i == curPos && curLine[i] == ' ') return "";
+                else
+                {
+                    if (curLine[i] == ' ' || curLine[i] == '\'') break;
+                    else
+                    {
+                        sb.Append(curLine[i]);
+                    }
+                }
+            }
+
+            // Scan Forward
+            for (int i = curPos + 1; i < curLine.Length; i++)
+            {
+                if (i == curPos && curLine[i] == ' ') break;
+                else
+                {
+                    if (curLine[i] == ' ' || curLine[i] == '\'') break;
+                    else
+                    {
+                        sb.Insert(0, curLine[i]);
+                    }
+                }
+            }
+
+            char[] buffer = sb.ToString().ToCharArray();
+            Array.Reverse(buffer);
+            string command = new string(buffer);
+            if (IBMiUtilities.IsValidQSYSObjectName(command))
+            {
+                return command;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        internal static void LoadFileCache()
+        {
+            CLCommands = new List<CLCommand>();
+            foreach (string file in Directory.GetFiles(Main.FileCacheDirectory))
+            {
+                if (!file.EndsWith(".cdml"))
+                {
+                    continue;
+                }
+
+                //TODO 
+            }
+        }
+
+        internal static void LoadCMD(string lookupString)
+        {
+            CLCommands.Add(new CLCommand(lookupString));
+
+        }
+    }
+}
