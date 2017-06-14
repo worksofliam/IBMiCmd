9cd215e1efd750d9ab993110ae0a16463bd95441
Liam Allan <mrliamallan@live.co.uk>
2017-06-14 12:52:18 +0100

    Fixed positioning of Library List window

--- Forms/libraryList.Designer.cs
+++ Forms/libraryList.Designer.cs
@@ -41,7 +41,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Location = new System.Drawing.Point(12, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(190, 20);
             this.label1.TabIndex = 0;
@@ -49,14 +49,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 114);
+            this.textBox1.Location = new System.Drawing.Point(11, 114);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(645, 26);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(576, 156);
+            this.button1.Location = new System.Drawing.Point(575, 172);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(81, 28);
             this.button1.TabIndex = 2;
@@ -66,7 +66,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(489, 156);
+            this.button2.Location = new System.Drawing.Point(486, 172);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(81, 28);
             this.button2.TabIndex = 3;
@@ -77,22 +77,22 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Location = new System.Drawing.Point(12, 146);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(169, 156);
+            this.textBox2.Location = new System.Drawing.Point(164, 173);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(307, 26);
+            this.textBox2.Size = new System.Drawing.Size(316, 26);
             this.textBox2.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Location = new System.Drawing.Point(7, 176);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(151, 20);
             this.label3.TabIndex = 6;
@@ -105,15 +105,15 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(574, 60);
             this.label4.TabIndex = 7;
-            this.label4.Text = "IBMiCmd needs a path on the remote server corresponding to your deveopment \r\nenvir" +
-    "onment in order to retrieve the correct information used for tools like auto \r\nco" +
-    "mpletion.";
+            this.label4.Text = "IBMiCmd needs a path on the remote server corresponding to your deveopment \r\nenvi" +
+    "ronment in order to retrieve the correct information used for tools like auto \r\n" +
+    "completion.";
             // 
             // libraryList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 198);
+            this.ClientSize = new System.Drawing.Size(668, 212);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -127,7 +127,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "libraryList";
-            this.Text = "libraryList";
+            this.Text = "Library List";
             this.Load += new System.EventHandler(this.libraryList_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
